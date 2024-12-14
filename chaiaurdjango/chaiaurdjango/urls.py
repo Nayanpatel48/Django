d86d9e11576e43c19dc4e19a27030b57f0cef642
nayan <www.xil187@gmail.com>
2024-12-14 16:18:07 +0530

    models done

--- chaiaurdjango/chaiaurdjango/urls.py
+++ chaiaurdjango/chaiaurdjango/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 from django.contrib import admin
+from django.conf import settings 
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +13,4 @@ urlpatterns = [
 
     # whenever i am hitting this url i am transfering control to an app urls
     path('chai/', include('chai.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
