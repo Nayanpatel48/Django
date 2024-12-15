bef9b2d1a99789b05b6262eea89486b834034639
nayan <www.xil187@gmail.com>
2024-12-15 10:18:36 +0530

    project done

--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -17,7 +17,7 @@ class ChaiVariety(models.Model):
     date_added = models.DateTimeField(default=timezone.now)
 
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
+    description = models.TextField(default='')
 
     def __str__(self):
         return self.name
-    
